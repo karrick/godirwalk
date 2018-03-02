5b763a787e8f57784fc7e6f91afd9c69af3bd6e9
Karrick S. McDermott <karrick@karrick.net>
2018-03-02 04:03:48 -0500

    builds with vgo

--- readdir_test.go
+++ readdir_test.go
@@ -35,6 +35,10 @@ func TestReadDirents(t *testing.T) {
 			modeType: os.ModeDir,
 		},
 		&Dirent{
+			name:     "dir6",
+			modeType: os.ModeDir,
+		},
+		&Dirent{
 			name:     "file3",
 			modeType: 0,
 		},
@@ -103,7 +107,7 @@ func TestReadDirnames(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	expected := []string{"dir1", "dir2", "dir3", "dir4", "dir5", "file3", "symlinks"}
+	expected := []string{"dir1", "dir2", "dir3", "dir4", "dir5", "dir6", "file3", "symlinks"}
 
 	if got, want := len(entries), len(expected); got != want {
 		t.Fatalf("(GOT) %v; (WNT) %v", got, want)
