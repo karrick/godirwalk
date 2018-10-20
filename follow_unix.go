1dfac3930d00f795933555e3c745bc6cff57b5f1
Chad Netzer <chad.netzer@gmail.com>
2018-10-20 03:08:47 -0700

    Add per-os EvalSymlinks wrapper
    Remove the os-specific EvalSymlinks() call from symlinkDirHelper() and
    put in their own per-os build files as a wrapped func.  This reduces
    code duplication, which will be important as additional changes are made
    to the symlink directory helper func.

---
+++ follow_unix.go
@@ -0,0 +1,6 @@
+package godirwalk
+
+// On unix we don't need to fixup pathnames with symlinks when doing a Stat().
+func evalSymlinksHelper(pathname string) (string, error) {
+	return pathname, nil
+}
