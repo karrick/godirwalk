6bd0f5cfa80132663ac1c88a804021abf4428c32
Karrick S. McDermott <karrick@karrick.net>
2019-05-08 12:47:50 +0000

    possible solaris support by falling back on Stat

--- readdir_unix.go
+++ readdir_unix.go
@@ -1,10 +1,9 @@
-// +build darwin freebsd linux netbsd openbsd
+// +build darwin freebsd linux netbsd openbsd solaris
 
 package godirwalk
 
 import (
 	"os"
-	"path/filepath"
 	"syscall"
 	"unsafe"
 )
@@ -49,39 +48,10 @@ func readdirents(osDirname string, scratchBuffer []byte) (Dirents, error) {
 			}
 			osChildname := string(nameSlice)
 
-			// Convert syscall constant, which is in purview of OS, to a
-			// constant defined by Go, assumed by this project to be stable.
-			var mode os.FileMode
-			switch de.Type {
-			case syscall.DT_REG:
-				// regular file
-			case syscall.DT_DIR:
-				mode = os.ModeDir
-			case syscall.DT_LNK:
-				mode = os.ModeSymlink
-			case syscall.DT_CHR:
-				mode = os.ModeDevice | os.ModeCharDevice
-			case syscall.DT_BLK:
-				mode = os.ModeDevice
-			case syscall.DT_FIFO:
-				mode = os.ModeNamedPipe
-			case syscall.DT_SOCK:
-				mode = os.ModeSocket
-			default:
-				// If syscall returned unknown type (e.g., DT_UNKNOWN, DT_WHT),
-				// then resolve actual mode by getting stat.
-				fi, err := os.Lstat(filepath.Join(osDirname, osChildname))
-				if err != nil {
-					_ = dh.Close() // ignore potential error returned by Close
-					return nil, err
-				}
-				// Even though the stat provided all file mode bits, we want to
-				// ensure same values returned to caller regardless of whether
-				// we obtained file mode bits from syscall or stat call.
-				// Therefore mask out the additional file mode bits that are
-				// provided by stat but not by the syscall, so users can rely on
-				// their values.
-				mode = fi.Mode() & os.ModeType
+			mode, err := fileModeFromDirent(de, osDirname, osChildname)
+			if err != nil {
+				_ = dh.Close() // ignore potential error returned by Close
+				return nil, err
 			}
 
 			entries = append(entries, &Dirent{name: osChildname, modeType: mode})
