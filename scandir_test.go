db564c86bce7d092f94343d2f99145c07314fe67
Karrick S. McDermott <karrick@karrick.net>
2020-01-27 16:41:15 -0500

    scanner test rewritten without readdirents
    Previously the Scanner tests were written in terms of calling
    ReadDirents and ReadDirnames, because those functions were implemented
    using Scanner. In this branch Scanner is written independent of those
    functions, so the test must actually call the function under test.

--- scandir_test.go
+++ scandir_test.go
@@ -37,41 +37,40 @@ func TestScannerDirent(t *testing.T) {
 }
 
 func TestScanDir(t *testing.T) {
-	t.Run("dirents", func(t *testing.T) {
-		actual, err := ReadDirents(filepath.Join(testRoot, "d0"), nil)
+	var actual []*Dirent
 
+	scanner, err := NewScanner(filepath.Join(testRoot, "d0"))
+	ensureError(t, err)
+
+	for scanner.Scan() {
+		dirent, err := scanner.Dirent()
 		ensureError(t, err)
+		actual = append(actual, dirent)
+	}
+	ensureError(t, scanner.Err())
 
-		expected := Dirents{
-			&Dirent{
-				name:     maxName,
-				modeType: os.FileMode(0),
-			},
-			&Dirent{
-				name:     "d1",
-				modeType: os.ModeDir,
-			},
-			&Dirent{
-				name:     "f1",
-				modeType: os.FileMode(0),
-			},
-			&Dirent{
-				name:     "skips",
-				modeType: os.ModeDir,
-			},
-			&Dirent{
-				name:     "symlinks",
-				modeType: os.ModeDir,
-			},
-		}
+	expected := Dirents{
+		&Dirent{
+			name:     maxName,
+			modeType: os.FileMode(0),
+		},
+		&Dirent{
+			name:     "d1",
+			modeType: os.ModeDir,
+		},
+		&Dirent{
+			name:     "f1",
+			modeType: os.FileMode(0),
+		},
+		&Dirent{
+			name:     "skips",
+			modeType: os.ModeDir,
+		},
+		&Dirent{
+			name:     "symlinks",
+			modeType: os.ModeDir,
+		},
+	}
 
-		ensureDirentsMatch(t, actual, expected)
-	})
-
-	t.Run("dirnames", func(t *testing.T) {
-		actual, err := ReadDirnames(filepath.Join(testRoot, "d0"), nil)
-		ensureError(t, err)
-		expected := []string{maxName, "d1", "f1", "skips", "symlinks"}
-		ensureStringSlicesMatch(t, actual, expected)
-	})
+	ensureDirentsMatch(t, actual, expected)
 }
